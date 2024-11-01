dd83057310aeb7c0a55a98b853a39a6e4ffc9f14
vitorBelo <vitorbelo99@gmail.com>
2024-11-01 01:28:49 -0300

    pages & components

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -7,11 +7,11 @@ export function AuthLayout() {
       <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
         <div className="flex items-center gap-3 text-lg text-foreground">
           <Pizza className="h-5 w-5" />
-          <span className="font-semibold">Pizza.Shop</span>
+          <span className="font-semibold">pizza.shop</span>
         </div>
 
         <footer className="text-sm">
-          Painel do parceiro &copy; Pizza.Shop - {new Date().getFullYear()}
+          Painel do parceiro &copy; pizza.shop - {new Date().getFullYear()}
         </footer>
       </div>
 
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,17 +1,17 @@
-import { Label } from '@radix-ui/react-label'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
-import { FiPlus } from 'react-icons/fi'
 import { Link } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
 
 const signInForm = z.object({
   email: z.string().email(),
 })
+
 type SignInForm = z.infer<typeof signInForm>
 
 export function SignIn() {
@@ -24,6 +24,7 @@ export function SignIn() {
   async function handleSignIn(data: SignInForm) {
     try {
       await new Promise((resolve) => setTimeout(resolve, 2000))
+
       toast.success('Enviamos um link de autenticação para seu e-mail.', {
         action: {
           label: 'Reenviar',
@@ -40,13 +41,12 @@ export function SignIn() {
   return (
     <>
       <Helmet title="Login" />
+
       <div className="p-8">
-        <Button variant={'ghost'} asChild className="absolute right-8 top-8">
-          <Link to="/sign-up" className="flex items-center gap-2">
-            Cadastrar estabelecimento
-            <FiPlus />
-          </Link>
+        <Button variant="ghost" asChild className="absolute right-8 top-8">
+          <Link to="/sign-up">Novo estabelecimento</Link>
         </Button>
+
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
@@ -57,10 +57,10 @@ export function SignIn() {
             </p>
           </div>
 
-          <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
+          <form className="space-y-4" onSubmit={handleSubmit(handleSignIn)}>
             <div className="space-y-2">
               <Label htmlFor="email">Seu e-mail</Label>
-              <Input id="email" type="email" {...register('email')}></Input>
+              <Input id="email" type="email" {...register('email')} />
             </div>
 
             <Button disabled={isSubmitting} className="w-full" type="submit">
